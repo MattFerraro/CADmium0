2b6ad64cd3e9d475302cbb43381b381cb593b919
Matt Ferraro <mferraro89@gmail.com>
2022-08-08 00:35:37 -0400

    move to the bottle example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 import "@google/model-viewer";
-import { visualizeShapes } from "./visualize.js";
+import { visualizeShapes, makeBottle } from "./visualize.js";
 import initOpenCascade from "opencascade.js";
 
 import { useState, useEffect } from 'react'
 
 function App() {
   const [ modelURL, setModelURL ] = useState("")
-  const [ radius, setRadius ] = useState(0.65)
+  const [ bwidth, setBWidth ] = useState(50)
+  const [ bheight, setBHeight ] = useState(70)
+  const [ bthickness, setBThickness ] = useState(30)
   const [ oc, setOC ] = useState()
   
   useEffect(() => {
@@ -24,22 +26,35 @@ function App() {
       return
     }
 
-    console.log("oc is not null!")
-    console.log("radius: ", radius)
+    // const box = new oc.BRepPrimAPI_MakeBox_2(1, 1, 1);
+    // const sphere = new oc.BRepPrimAPI_MakeSphere_5(new oc.gp_Pnt_3(0.5, 0.5, 0.5), radius);
+    // const cut = new oc.BRepAlgoAPI_Cut_3(box.Shape(), sphere.Shape(), new oc.Message_ProgressRange_1());
+    // cut.Build(new oc.Message_ProgressRange_1());
+    // const mUrl = visualizeShapes(oc, cut.Shape());
+    // setModelURL(mUrl)
 
-    const box = new oc.BRepPrimAPI_MakeBox_2(1, 1, 1);
-    const sphere = new oc.BRepPrimAPI_MakeSphere_5(new oc.gp_Pnt_3(0.5, 0.5, 0.5), radius);
-    const cut = new oc.BRepAlgoAPI_Cut_3(box.Shape(), sphere.Shape(), new oc.Message_ProgressRange_1());
-    cut.Build(new oc.Message_ProgressRange_1());
-    const mUrl = visualizeShapes(oc, cut.Shape());
+    const bottleShape = makeBottle(oc, bwidth, bheight, bthickness)
+    const mUrl = visualizeShapes(oc, bottleShape);
     setModelURL(mUrl)
-  }, [oc, radius])
+
+  }, [oc, bwidth, bheight, bthickness])
 
 
   return (
     <div className="App">
-      <div>Model goes here! {modelURL}</div>
-      <input type="range" min="0.55" max=".80" step="0.01" value={radius} onChange={e => setRadius(e.target.value)} className="slider" id="myRange"></input>
+      <div>
+        Width
+        <input type="range" min="20" max="100" step="1" value={bwidth} onChange={e => setBWidth(e.target.value)} className="slider" id="myRange"></input>
+      </div>
+      
+      <div>
+        Height
+        <input type="range" min="50" max="120" step="1" value={bheight} onChange={e => setBHeight(e.target.value)} className="slider" id="myRange"></input>
+      </div>
+      <div>
+        Thickness
+        <input type="range" min="15" max="50" step="1" value={bthickness} onChange={e => setBThickness(e.target.value)} className="slider" id="myRange"></input>
+      </div>
       <model-viewer style={{width:600, height:500}} src={modelURL} camera-controls enable-pan />
     </div>
   );
