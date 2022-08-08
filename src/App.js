b0a4e9f62260951bb996f737685d5b7b4a5e18c0
Matt Ferraro <mferraro89@gmail.com>
2022-08-07 23:59:32 -0400

    Make the radius configurable

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 import "@google/model-viewer";
 import { visualizeShapes } from "./visualize.js";
-import initOpenCascade, { BRepMesh_ModelBuilder } from "opencascade.js";
+import initOpenCascade from "opencascade.js";
 
 import { useState, useEffect } from 'react'
 
 function App() {
   const [ modelURL, setModelURL ] = useState("")
+  const [ radius, setRadius ] = useState(0.65)
+  const [ oc, setOC ] = useState()
   
   useEffect(() => {
     console.log("Starting to import openCascade wasm...")
-    initOpenCascade().then((oc) => {
+    initOpenCascade().then((newOC) => {
       console.log("wasm is initialized!")
-      const sphereSize = .65;
-      const box = new oc.BRepPrimAPI_MakeBox_2(1, 1, 1);
-      console.log("I have a box!")
-      const sphere = new oc.BRepPrimAPI_MakeSphere_5(new oc.gp_Pnt_3(0.5, 0.5, 0.5), sphereSize);
-      console.log("I have a sphere!")
-      const cut = new oc.BRepAlgoAPI_Cut_3(box.Shape(), sphere.Shape(), new oc.Message_ProgressRange_1());
-      console.log("I have cut the sphere out of the box!")
-      cut.Build(new oc.Message_ProgressRange_1());
-      console.log("I have built the cut geometry")
-      const mUrl = visualizeShapes(oc, cut.Shape());
-      console.log("I have a visualizable model!")
-      setModelURL(mUrl)
-    });    
-  }, [])
+      setOC(newOC)
+    });
+  }, [setOC])
+
+  useEffect(() => {
+    if (oc == null) {
+      return
+    }
+
+    console.log("oc is not null!")
+    console.log("radius: ", radius)
+
+    const box = new oc.BRepPrimAPI_MakeBox_2(1, 1, 1);
+    const sphere = new oc.BRepPrimAPI_MakeSphere_5(new oc.gp_Pnt_3(0.5, 0.5, 0.5), radius);
+    const cut = new oc.BRepAlgoAPI_Cut_3(box.Shape(), sphere.Shape(), new oc.Message_ProgressRange_1());
+    cut.Build(new oc.Message_ProgressRange_1());
+    const mUrl = visualizeShapes(oc, cut.Shape());
+    setModelURL(mUrl)
+  }, [oc, radius])
 
 
   return (
     <div className="App">
       <div>Model goes here! {modelURL}</div>
+      <input type="range" min="0.55" max=".80" step="0.01" value={radius} onChange={e => setRadius(e.target.value)} className="slider" id="myRange"></input>
       <model-viewer style={{width:600, height:500}} src={modelURL} camera-controls enable-pan />
     </div>
   );
