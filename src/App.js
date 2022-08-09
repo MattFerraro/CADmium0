55bea3382ee9c1f504afac825784686a9928fc99
Matt Ferraro <mferraro89@gmail.com>
2022-08-08 22:43:14 -0400

    Add SVGs for the first round of action buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 
-import logo from '../public/logo192.png'
+import sketch from './svgs/sketch_min.svg'
+import extrude from './svgs/extrude_min.svg'
+import fillet from './svgs/fillet_min.svg'
+import chamfer from './svgs/chamfer_min.svg'
+import draft from './svgs/draft_min.svg'
+import hole from './svgs/hole_min.svg'
+import loft from './svgs/loft_min.svg'
+import mirrorBody from './svgs/mirrorBody_min.svg'
+import patternLinear from './svgs/patternLinear_min.svg'
+import patternRadial from './svgs/patternRadial_min.svg'
+import plane from './svgs/plane_min.svg'
+import revolve from './svgs/revolve_min.svg'
+import scale from './svgs/scale_min.svg'
+import shell from './svgs/shell_min.svg'
+import sweep from './svgs/sweep_min.svg'
+import boolean from './svgs/boolean_min.svg'
 
 import "@google/model-viewer";
 import { visualizeShapes, makeBottle } from "./visualize.js";
@@ -19,6 +34,8 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+
+const actionIconWidth = 35
 
 function App() {
   const [ modelURL, setModelURL ] = useState("nope")
@@ -73,19 +90,138 @@ function App() {
         <AppBar position="static" color='grey' elevation={0}>
           <Toolbar variant='dense'>
             <IconButton
-              size="large"
-              edge="start"
+              size="small"
               color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
+              aria-label="sketch"
             >
-              <MenuIcon />
+              <img src={sketch} alt='sketch' height={actionIconWidth} width={actionIconWidth}/>
             </IconButton>
+            
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="extrude"
+            >
+              <img src={extrude} alt="extrude" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+            
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="revolve"
+            >
+              <img src={revolve} alt="revolve" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="sweep"
+            >
+              <img src={sweep} alt="sweep" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="loft"
+            >
+              <img src={loft} alt="loft" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+            
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="fillet"
+            >
+              <img src={fillet} alt="fillet" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="chamfer"
+            >
+              <img src={chamfer} alt="chamfer" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+            
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="draft"
+            >
+              <img src={draft} alt="draft" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="shell"
+            >
+              <img src={shell} alt="shell" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="hole"
+            >
+              <img src={hole} alt="hole" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="patternLinear"
+            >
+              <img src={patternLinear} alt="patternLinear" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="patternRadial"
+            >
+              <img src={patternRadial} alt="patternRadial" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="mirrorBody"
+            >
+              <img src={mirrorBody} alt="mirrorBody" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="boolean"
+            >
+              <img src={boolean} alt="boolean" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="scale"
+            >
+              <img src={scale} alt="scale" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
+            <IconButton
+              size="small"
+              color="inherit"
+              aria-label="plane"
+            >
+              <img src={plane} alt="plane" height={actionIconWidth} width={actionIconWidth}/>
+            </IconButton>
+
           </Toolbar>
         </AppBar>
       </Box>
 
-      <Button variant="contained">Hello World</Button>
       <div>
         Width
         <input type="range" min="20" max="100" step="1" value={bwidth} onChange={e => setBWidth(e.target.value)} className="slider" id="myRange"></input>
